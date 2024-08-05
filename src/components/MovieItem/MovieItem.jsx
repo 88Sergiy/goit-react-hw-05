4aacd6cced6a52a4cde15e2a4782c286c73f4b9d
88Sergiy <152657236+88Sergiy@users.noreply.github.com>
2024-08-05 19:56:54 +0300

    commit-finish

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -3,46 +3,45 @@ import { Link, useLocation } from 'react-router-dom';
 import { imagePathBase } from '../../api/tmdb';
 
 function MovieItem({ movie }) {
-    const location = useLocation();
+  const location = useLocation();
 
-    return (
-        <div className="card">
-            <link to={'/movies/' + movie.id} state={location}>
-                <div className="image-wrapper h-96 relative rounded-t-lg owerflow-hidden">
-                    {movie.adult && (
-                        <div
-                            className="adult rouded-full bg-red-500 w-14 h-14"
-                            flex justify-center items-center rext-x1 text-neutral-200
-                            font-bold absolute top-3 right-3 shadow-md
-                        >
-                            <span>18+</span>
-               
-                        </div>
-                    )}
-                    <img
-                        src={imagePathBase + '/' + movie.poster_path}
-                        alt={movie.title}
-                        className="w-full h-full object-cover"
-                    />
-                    <div
-                        className="rating rounded-md text-neutral-100 text-xl 
-                        bg-slate-800/70 px-8 py-3 absolute bottom-2 left-[50%] translate-x-[-50%]
-                        2x1:py-2 2x1:px-4"
-                    >
-                        Rate:{' '}
-                        <span className='text-yellow-400'>
-                            {movie.vote_average.toFixed(1)}
-                        </span>
-                    </div>
-                </div>
-                <div className="card-title text-xl text-center my-3">{movie.title}</div>
-            </link>
+  return (
+    <div className="card">
+      <Link to={'/movies/' + movie.id} state={location}>
+        <div className="image-wrapper h-96 relative rounded-t-lg overflow-hidden">
+          {movie.adult && (
+            <div
+              className="adult rounded-full bg-red-500 w-14 h-14
+            flex justify-center items-center text-xl text-neutral-200 
+            font-bold absolute top-3 right-3 shadow-md"
+            >
+              <span>18+</span>
+            </div>
+          )}
+          <img
+            src={imagePathBase + '/' + movie.poster_path}
+            alt={movie.title}
+            className="w-full h-full object-cover"
+          />
+          <div
+            className="rating rounded-md text-neutral-100 text-xl
+        bg-slate-800/70 px-8 py-3 absolute bottom-2 left-[50%] translate-x-[-50%]
+        2xl:py-2 2xl:px-4"
+          >
+            Rate:{' '}
+            <span className="text-yellow-400">
+              {movie.vote_average.toFixed(1)}
+            </span>
+          </div>
         </div>
-    );
+        <div className="card-title text-xl text-center my-3">{movie.title}</div>
+      </Link>
+    </div>
+  );
 }
 
 MovieItem.propTypes = {
-    movie: PropTypes.object,
+  movie: PropTypes.object,
 };
 
 export default MovieItem;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,23 +2,24 @@ import PropTypes from 'prop-types';
 
 import MovieItem from '../MovieItem/MovieItem';
 
-function MovieList({ Movies }) {
-    return (
-        <ul className="flex gap-5 flex-wrap bg-violet-100 py-8 px-6 rounded-xl">
-            {Movies.map(movie => (
-                <li
-                    key={movie.id}
-                    className="flex auto basis-full sm:basis-1/3 lg:basis-1/4
-                xl:basis-1/5 2x1:basis-1/6 bg-white rouded-lg"
-                >
-                    <MovieItem movie={movie} />
-                </li>
-            ))}
-        </ul>
-    );
+function MovieList({ movies }) {
+  return (
+    <ul className="flex gap-5 flex-wrap bg-violet-100 py-8 px-6 rounded-xl">
+      {movies.map(movie => (
+        <li
+          key={movie.id}
+          className="flex-auto basis-full sm:basis-1/3 lg:basis-1/4 
+              xl:basis-1/5 2xl:basis-1/6 bg-white rounded-lg"
+        >
+          <MovieItem movie={movie} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 MovieList.propTypes = {
-    movies: PropTypes.array,
+  movies: PropTypes.array,
 };
+
 export default MovieList;
