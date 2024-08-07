108869fecdb3c0856977e7be32589c19aca8fa08
88Sergiy <152657236+88Sergiy@users.noreply.github.com>
2024-08-07 07:07:33 +0300

    commit-9

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -124,6 +124,25 @@ function MovieDetailsPage() {
                 </div>
               </div>
             </div>
+
+            <nav className="my-8">
+              <NavLink
+                to="cast"
+                className="mr-4 text-violet-500 hover:text-violet-700"
+              >
+                Cast
+              </NavLink>
+              <NavLink
+                to="reviews"
+                className="text-violet-500 hover:text-violet-700"
+              >
+                Reviews
+              </NavLink>
+            </nav>
+
+            <Suspense fallback={<div>Loading...</div>}>
+              <Outlet />
+            </Suspense>
           </>
         )}
       </div>
